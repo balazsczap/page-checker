4b0edfce0cbf6faa5027da6c4021fc3818379569
Czap Balázs <balazsczap@gmail.com>
2018-05-20 00:41:24 +0200

    added parameters file, renamed some parameters

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const program = require('commander');
+const fs = require('fs');
 
 const defaults = {
   interval: 1000 * 60 * 5,
   sourceEmail: 'noreply@balazsczap.com',
   port: 3000,
   cacheDir: './.cached/',
+  selector: 'body',
 };
 
 program
@@ -13,16 +15,21 @@ program
   .option('-s, --source <source>', 'Source email address', defaults.sourceEmail)
   .option('-p, --port <port>', 'Port for express server when using in server mode', defaults.port)
   .option('-c, --cache-dir <cacheDir>', 'Directory path for caching websites', defaults.cacheDir)
-  .option('-t, --target <target>', 'Target email address when using CLI mode')
+  .option('-S, --selector <selector>', 'Target url when using in CLI mode', defaults.selector)
+  .option('-P, --param-file <paramFile>', 'JSON file in which run parameters are specified')
+  .option('-S, --subscriber <subscriber>', 'Subscriber email address when using CLI mode')
   .option('-u, --url <url>', 'Target url when using in CLI mode')
   .parse(process.argv);
 
+const { url, port, subscriber, selector } = (program.paramFile ?
+  fs.readFileSync(program.paramFile) : program);
 
 module.exports = {
   interval: program.interval,
   source: program.source,
   cacheDir: program.cacheDir,
-  target: program.target,
-  url: program.url,
-  port: program.port,
+  url,
+  port,
+  subscriber,
+  selector,
 };
