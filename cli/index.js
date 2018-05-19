8dbb82db93cff7d43ba1c5a65f6f4bca18ec1181
Czap Balázs <balazsczap@gmail.com>
2018-05-20 01:23:40 +0200

    Fix not renamed parameter

--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,9 @@
 process.env.DEBUG = 'page-checker:*';
 
-const { target, url, selector } = require('../config');
+const { subscriber, url, selector } = require('../config');
 const { subscribeToUrl } = require('../service');
 const info = require('debug')('page-checker:info');
 
-subscribeToUrl(target, url, selector);
-info(`Subscribed ${target} to ${url}`);
+subscribeToUrl(subscriber, url, selector);
+info(`Subscribed ${subscriber} to ${url}`);
 
